77de8f5ae28a77de4ed37ac230add12320ce6251
root <root@ip-172-31-47-211.us-west-2.compute.internal>
2017-10-27 09:37:48 +0000

    anandBrach changes Oct 27 2017

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
 
+echo "anandBrach";
 echo "hello world.This is for pushing file to the master branch";
+
+echo "this is the test for anandBranch should see whether this clones to the master branch";
+
+
 
 ?>
