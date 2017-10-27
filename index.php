83206a637e743d263b5c1481f876e749039764be
root <root@ip-172-31-47-211.us-west-2.compute.internal>
2017-10-27 11:22:36 +0000

    test

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 
-<<<<<<< HEAD
+
 echo "anandBrach";
 echo "hello world.This is for pushing file to the master branch";
-=======
-echo "hello world.This is for pushing file to the master branchs;
->>>>>>> cfe2c3974302fd64bf5c3c3dfcb031dac9afd1cc
+
+echo "hello world.This is for pushing file to the master branchs";
+
 
 echo "this is the test for anandBranch should see whether this clones to the master branch";
 
